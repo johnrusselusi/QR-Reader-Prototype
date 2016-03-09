19b1e7dbd5e5e0ebc20833eaadae0e787b36ce15
John Russel Usi <j.usi@klab.com>
2016-03-09 11:53:52 +0800

    Finished the main feature.

--- QRReaderPrototype/QRReaderPrototype/ViewController.h
+++ QRReaderPrototype/QRReaderPrototype/ViewController.h
@@ -7,9 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+@import AVFoundation;
 
 @interface ViewController : UIViewController
-
 
 @end
 
--- QRReaderPrototype/QRReaderPrototype/ViewController.m
+++ QRReaderPrototype/QRReaderPrototype/ViewController.m
@@ -7,21 +7,83 @@
 //
 
 #import "ViewController.h"
+#import "SVProgressHUD.h"
+#import "AFNetworking.h"
 
-@interface ViewController ()
+@interface ViewController () <AVCaptureMetadataOutputObjectsDelegate>
+
+@property (nonatomic, strong) IBOutlet UIView *readerView;
+@property (nonatomic, strong) AVCaptureSession *session;
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(progressHUDDismissed) name:SVProgressHUDDidDisappearNotification object:nil];
+    
+    [self setupReaderView];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)setupReaderView
+{
+    self.session = [[AVCaptureSession alloc] init];
+    AVCaptureDevice *device = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    NSError *error = nil;
+    
+    AVCaptureVideoPreviewLayer *previewLayer = [AVCaptureVideoPreviewLayer layerWithSession:self.session];
+    
+    previewLayer.videoGravity = AVLayerVideoGravityResizeAspectFill;
+    previewLayer.frame = self.readerView.bounds;
+    [self.readerView.layer addSublayer:previewLayer];
+    
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:device error:&error];
+    
+    if (input)
+        [self.session addInput:input];
+    else
+        NSLog(@"Error %@", error);
+    
+    AVCaptureMetadataOutput *output = [[AVCaptureMetadataOutput alloc] init];
+    [output setMetadataObjectsDelegate:self queue:dispatch_get_main_queue()];
+    [self.session addOutput:output];
+    
+    [output setMetadataObjectTypes:@[AVMetadataObjectTypeQRCode, AVMetadataObjectTypeEAN13Code]];
+    
+    [self.session startRunning];
+}
+
+- (void)captureOutput:(AVCaptureOutput *)captureOutput didOutputMetadataObjects:(NSArray *)metadataObjects fromConnection:(AVCaptureConnection *)connection
+{
+    [self.session stopRunning];
+    NSString *decodedString = [[metadataObjects valueForKey:@"stringValue"] objectAtIndex:0];
+    [self getRequestForDecodedString:decodedString];
+}
+
+- (void)progressHUDDismissed
+{
+    [self.session startRunning];
+}
+
+- (void)getRequestForDecodedString:(NSString *)decodedString
+{
+    [SVProgressHUD showWithMaskType:SVProgressHUDMaskTypeGradient];
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    [manager GET:decodedString
+      parameters:nil
+        progress:nil
+         success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject)
+    {
+        [SVProgressHUD showInfoWithStatus:@"Success"];
+    }
+         failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error)
+     {
+         [SVProgressHUD showErrorWithStatus:@"Error"];
+     }];
 }
 
 @end
