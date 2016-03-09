6a963d9d23324e07469fe33c8c0b76090a04b532
John Russel Usi <j.usi@klab.com>
2016-03-09 09:39:17 +0800

    Initial project setup.

---
+++ QRReaderPrototype/QRReaderPrototype/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRReaderPrototype
+//
+//  Created by John Russel Usi on 3/9/16.
+//  Copyright © 2016 CYS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
